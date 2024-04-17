96784ecf078c4f2a9929e267e7d5333596279029
elias1235813 <elias.vuorenmaa706@gmail.com>
2024-04-17 13:31:50 +0300

    Adminsivun elokuvanäkymä
    Adminsivun elokuvanäkymä tehty. Nyt adminsivu näyttää myös elokuvat tietokannasta

--- frontend/src/components/Admin/AdmininElokuvaKortti.js
+++ frontend/src/components/Admin/AdmininElokuvaKortti.js
@@ -1,27 +1,19 @@
 import React from 'react';
 
-const AdmininElokuvaKortti = ({
-  id,
-  kuvaURL,
-  ohjaaja,
-  nimi,
-  julkaisuvuosi,
-  kuvaus,
-  arviot,
-}) => {
+const AdmininElokuvaKortti = ({ image, director, title, year, description, rating }) => {
   return (
     <div class="card mb-3">
       <div class="row g-0">
         <div class="col-md-4">
-          <img src={kuvaURL} class="img-fluid rounded-start" alt={nimi} />
+          <img src={image} class="img-fluid rounded-start" alt={title} />
         </div>
         <div class="col-md-8">
           <div class="card-body">
-            <h5 className="card-title">Nimi: {nimi}</h5>
-            <p className="card-text">Ohjaaja: {ohjaaja}</p>
-            <p className="card-text">Julkaisuvuosi: {julkaisuvuosi}</p>
-            <p className="card-text">Kuvaus: {kuvaus}</p>
-            <p className="card-text">Arviot: {arviot}</p>
+            <h5 className="card-title">Nimi: {title}</h5>
+            <p className="card-text">Ohjaaja: {director}</p>
+            <p className="card-text">Julkaisuvuosi: {year}</p>
+            <p className="card-text">Kuvaus: {description}</p>
+            <p className="card-text">Arviot: {rating}</p>
             <button className="btn btn-outline-success">Muokkaa</button>
             <button className="btn btn-outline-success">Poista</button>
           </div>
--- frontend/src/components/Admin/AdmininElokuvaLista.js
+++ frontend/src/components/Admin/AdmininElokuvaLista.js
@@ -1,47 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import AdmininElokuvaKortti from './AdmininElokuvaKortti';
 
-const AdmininElokuvaLista = () => {
-  const [leffat, setLeffat] = useState([
-    {
-      // Esimerkkidataa, joka korvataan kannan tiedoilla
-      id: 1,
-      nimi: 'Titanic',
-      ohjaaja: 'Joku jota en muista',
-      julkaisuvuosi: 1997,
-      kuvaus: 'Laiva uppoaa ja ihmisiä kuolee.',
-      arviot: 5,
-      kuvaURL:
-        'https://th.bing.com/th/id/R.08b8b82ae2176593cd91a089e4cc2597?rik=xKozU2PKX%2fC1Vg&pid=ImgRaw&r=0',
-    },
+function AdmininElokuvaLista(){
+  const [leffat, setMovies] = useState([]);
 
-    {
-      id: 2,
-      nimi: 'Leijonakuningas',
-      ohjaaja: 'Joku jota en muista',
-      julkaisuvuosi: 1994,
-      kuvaus: 'Leijonat käyvät valtataistelua ja Mufasa kuolee.',
-      arviot: 8.8,
-      kuvaURL:
-        'https://elokuvalisenssi.fi/wp-content/uploads/2021/07/u0ayQBxXKajRMl1tfGCPSoC3vBg-scaled-1.jpg',
-    },
-  ]);
-  // Varsinainen kortti:
+  useEffect(() => {
+    fetchData();
+  }, []); // Empty dependency array means this effect will run only once after the initial render
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch('/api/movies');
+      const data = await response.json();
+      setMovies(data);
+    } catch (error) {
+      console.error('Elokuvien hakeminen epäonnistui:', error);
+    }
+  };
+  
   return (
     <section className="movie-list">
       {leffat.map((leffa) => (
         <AdmininElokuvaKortti
-          id={leffa.id}
-          nimi={leffa.nimi}
-          ohjaaja={leffa.ohjaaja}
-          julkaisuvuosi={leffa.julkaisuvuosi}
-          kuvaus={leffa.kuvaus}
-          arviot={leffa.arviot}
-          kuvaURL={leffa.kuvaURL}
+          key={leffa._id}
+          _id={leffa._id}
+          image={leffa.image}
+          title={leffa.title}
+          director={leffa.director}
+          year={leffa.year}
+          rating={leffa.rating}
+          description={leffa.description}
         />
       ))}
     </section>
   );
-};
+
+}
 
 export default AdmininElokuvaLista;
