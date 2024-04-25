d3803afa48a4ba09cfdf6b523e03d1ae28c8dfbe
elias1235813 <elias.vuorenmaa706@gmail.com>
2024-04-25 10:34:38 +0300

    Accessibility korjauksia
    Aria labeleilta ja roleja lisätty. Näiden lisäksi muutama html elementti

--- frontend/src/components/Etusivu/EtusivunInfo.js
+++ frontend/src/components/Etusivu/EtusivunInfo.js
@@ -3,11 +3,11 @@ import React from 'react';
 const EtusivunInfo = () => {
   return (
     <article>
-      <h1>Tervetuloa maailman hienoimpaan elokuvatietokantaan!</h1>
-      <p>
-        {' '}
-        Sivustolla voit etsiä tietokantaan tallennettuja elokuvia ja saada
-        niistä listätietoa.
+      <h1 role="region" aria-label="Tervetuloa maailman hienoimpaan elokuvatietokantaan!">
+        Tervetuloa maailman hienoimpaan elokuvatietokantaan!
+      </h1>
+      <p role="region" aria-label="Sivustolla voit etsiä tietokantaan tallennettuja elokuvia ja saada niistä listätietoa.">
+        Sivustolla voit etsiä tietokantaan tallennettuja elokuvia ja saada niistä listätietoa.
       </p>
     </article>
   );
--- frontend/src/components/Etusivu/HakuJaSuodatus.js
+++ frontend/src/components/Etusivu/HakuJaSuodatus.js
@@ -9,21 +9,24 @@ const HakuJaSuodatus = ({ onSearch }) => {
   };
 
   return (
-    <section className="search-and-filter-area">
-      <h2>Haku</h2>
+    <section className="search-and-filter-area" role="search">
+      <h1>Haku</h1>
       <p> Etsi elokuvia kategorian mukaan</p>
       <form onSubmit={handleSearch}>
-        <input size="50"
+        <label htmlFor="genreInput">Kirjoita kategoria:</label>
+        <input
+          id="genreInput"
+          size="50"
           type="text"
           placeholder="Kirjoita kategoria..."
           name="etsi"
           value={genre}
           onChange={(e) => setGenre(e.target.value)}
+          aria-label="Kirjoita kategoria"
         />
         <button type="submit" className="btn btn-primary">
           Etsi
         </button>
-
       </form>
     </section>
   );
